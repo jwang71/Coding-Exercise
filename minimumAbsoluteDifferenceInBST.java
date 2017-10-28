4b590d8368d42fdfe74224ea11f8269724bb9842
kikihuaiting <jiaqiwang827@gmail.com>
2017-10-28 11:10:15 -0700

    lc530

---
+++ minimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int min = Integer.MAX_VALUE;
+    TreeNode prev;
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) return min;
+        getMinimumDifference(root.left);
+        
+        if (prev != null) {
+            min = Math.min(min, root.val - prev.val);
+        }
+        prev = root;
+        getMinimumDifference(root.right);
+        return min;
+    }
+}
+
+
+//Follow up, if the tree is not BST
+public class Solution {
+    TreeSet<Integer> set = new TreeSet<>();
+    int min = Integer.MAX_VALUE;
+    
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) return min;
+        
+        if (!set.isEmpty()) {
+            if (set.floor(root.val) != null) {
+                min = Math.min(min, root.val - set.floor(root.val));
+            }
+            if (set.ceiling(root.val) != null) {
+                min = Math.min(min, set.ceiling(root.val) - root.val);
+            }
+        }
+        
+        set.add(root.val);
+        
+        getMinimumDifference(root.left);
+        getMinimumDifference(root.right);
+        
+        return min;
+    }
+}
