4e41ae36d52a3d4b0556384778b6731b0f219072
kikihuaiting <jiaqiwang827@gmail.com>
2017-11-30 20:59:48 -0500

    update

--- buyStock.java
+++ buyStock.java
@@ -45,35 +45,29 @@ public int maxProfitIII(int[] array) {
 	return maxProfit;
 }
 
-public int maxProfitIV(int[] prices, int k) {
-    if (k == 0) {
-        return 0;
-    }
-    if (k >= prices.length / 2) {
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
-            }
-        }
-        return profit;
-    }
-    int n = prices.length;
-    int[][] mustsell = new int[n + 1][n + 1];   // mustSell[i][j] 表示前i天，至多进行j次交易，第i天必须sell的最大获益
-    int[][] globalbest = new int[n + 1][n + 1];  // globalbest[i][j] 表示前i天，至多进行j次交易，第i天可以不sell的最大获益
-    
-    mustsell[0][0] = globalbest[0][0] = 0;
-    // for (int i = 1; i <= k; i++) {
-    //     mustsell[0][i] = globalbest[0][i] = 0;
-    // }
-    
-    for (int i = 1; i < n; i++) {
-        int gainorlose = prices[i] - prices[i - 1];
-        for (int j = 1; j <= k; j++) {
-            mustsell[i][j] = Math.max(globalbest[(i - 1)][j - 1] + Math.max(0,gainorlose),
-                                        mustsell[(i - 1)][j] + gainorlose);
-            globalbest[i][j] = Math.max(globalbest[(i - 1)][j], mustsell[i ][j]);
-        }
-    }
-    return globalbest[(n - 1)][k];
+public int maxProfit(int[] array, int k) {
+	int len = array.length;
+	if (k == 0) return 0;
+	if (k >= len/2) {
+	  int profit = 0;
+	  for (int i = 1; i < len; i++) {
+	    if (array[i] > array[i - 1]) {
+	      profit += array[i] - array[i - 1];
+	    }
+	  }
+	  return profit;
+	}
+
+	int[][] local = new int[len][k + 1];
+	int[][] global = new int[len][k + 1];
+	local[0][0] = global[0][0] = 0;
+
+	for (int i = 1; i < len; i++) {
+	  int gain = array[i] - array[i - 1];
+	  for (int j = 1; j <= k; j++) {
+	    local[i][j] = Math.max(global[i-1][j-1] + Math.max(0, gain), local[i-1][j]+gain);
+	    global[i][j] = Math.max(global[i - 1][j], local[i][j]);
+	  }
+	}
+	return global[len-1][k];
 }
