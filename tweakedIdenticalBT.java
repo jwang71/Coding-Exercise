4a8b6bcacea6d6a01448832f8c1a60afb65ddb24
kikihuaiting <jiaqiwang827@gmail.com>
2017-12-08 14:38:58 -0500

    delete

--- tweakedIdenticalBT.java
+++
@@ -1,8 +0,0 @@
-  public boolean isTweakedIdentical(TreeNode one, TreeNode two) {
-    // Write your solution here.
-    if (one == null && two == null) return true;
-    if (one == null || two == null || one.key != two.key) return false;
-    
-    return (isTweakedIdentical(one.left, two.left) && isTweakedIdentical(one.right, two.right) )
-    || (isTweakedIdentical(one.left, two.right)&&isTweakedIdentical(one.right, two.left));
-  }
