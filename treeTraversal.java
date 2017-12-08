a1ed5fb9cc635bba7b49f4e9c8d207c2604e20d1
kikihuaiting <jiaqiwang827@gmail.com>
2017-12-08 14:27:01 -0500

    4-1-2-3

---
+++ treeTraversal.java
@@ -0,0 +1,97 @@
+//tree traversal iteratively
+  public List<Integer> inOrder(TreeNode root) {
+    List<Integer> list  = new ArrayList<>();
+    if (root == null) return list;
+    
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode cur = root;
+    
+    while (cur != null || !stack.isEmpty()) {
+      while (cur != null) {
+        stack.push(cur);
+        cur = cur.left;
+      }
+      cur = stack.pop();
+      list.add(cur.key);
+      cur = cur.right;
+    }
+    return list;
+  }
+
+  public List<Integer> preOrder(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) return res;
+    Stack<TreeNode> stack = new Stack<>();
+    
+    stack.push(root);
+    while (!stack.isEmpty()) {
+      TreeNode cur = stack.pop();
+      res.add(cur.key);
+      if (cur.right != null) {
+        stack.push(cur.right);
+      }
+      if (cur.left != null) {
+        stack.push(cur.left);
+      }
+    }
+    return res;
+  }
+
+  public List<Integer> postOrder(TreeNode root) {
+    List<Integer> res = new ArrayList<Integer>();
+    if (root == null) return res;
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      TreeNode cur = stack.pop();
+      res.add(0, cur.key);
+      if (cur.left != null) {
+        stack.push(cur.left);
+      }
+      if (cur.right != null) {
+        stack.push(cur.right);
+      }
+    }
+    return res;
+  }
+
+//recursion
+  public List<Integer> inOrder(TreeNode root) {
+    // Write your solution here.
+    List<Integer> res = new ArrayList<>();
+    if (root == null) return res;
+    List<Integer> left = inOrder(root.left);
+    List<Integer> right = inOrder(root.right);
+
+    res.addAll(left);
+    res.add(root.key);
+
+    res.addAll(right);
+    return res;
+  }
+  
+  public List<Integer> preOrder(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) return res;
+    List<Integer> left = preOrder(root.left);
+    List<Integer> right = preOrder(root.right);
+    res.add(root.key);
+
+    res.addAll(left);
+    res.addAll(right);
+    return res;
+  }
+
+  public List<Integer> postOrder(TreeNode root) {
+    List<Integer> res = new ArrayList<Integer>();
+    if (root == null) return res;
+    
+    List<Integer> left = postOrder(root.left);
+    List<Integer> right = postOrder(root.right);
+    
+    res.addAll(left);
+    res.addAll(right);
+    res.add(root.key);
+    return res;
+  }
